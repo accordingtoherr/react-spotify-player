29f1c7f5f697ada0dd4f690898b68fa5fe7ebee2
accordingtoherr <emilyherr94@gmail.com>
2020-12-01 23:01:13 -0500

    added api

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import * as $ from "jquery";
 import { authEndpoint, clientId, redirectUri, scopes } from "./config";
 import hash from "./hash";
 import Player from "./Player";
-
+import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -20,10 +20,16 @@ class App extends Component {
         duration_ms: 0
       },
       is_playing: "Paused",
-      progress_ms: 0
+      progress_ms: 0,
+      no_data: false,
     };
-    this.getCurrentlyPlaying= this.getCurrentlyPlaying.bind(this);
+
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+    this.tick = this.tick.bind(this);
   }
+
+
+
   componentDidMount() {
     // Set token
     let _token = hash.access_token;
@@ -35,35 +41,56 @@ class App extends Component {
       });
       this.getCurrentlyPlaying(_token);
     }
-  
+
+    // set interval for polling every 5 seconds
+    this.interval = setInterval(() => this.tick(), 5000);
   }
 
-getCurrentlyPlaying(token) {
-  
- $.ajax({
-       url: "https://api.spotify.com/v1/me/player",
-       type: "GET",
-       beforeSend: xhr => {
-         xhr.setRequestHeader("Authorization", "Bearer " + token);
-       },
+  componentWillUnmount() {
+    // clear the interval to save resources
+    clearInterval(this.interval);
+  }
+
+  tick() {
+    if(this.state.token) {
+      this.getCurrentlyPlaying(this.state.token);
+    }
+  }
+
+
+  getCurrentlyPlaying(token) {
+    // Make a call using the token
+    $.ajax({
+      url: "https://api.spotify.com/v1/recommendations",
+      type: "GET",
+      beforeSend: xhr => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
       success: data => {
+        // Checks if the data is not empty
+        if(!data) {
+          this.setState({
+            no_data: true,
+          });
+          return;
+        }
+
         this.setState({
           item: data.item,
           is_playing: data.is_playing,
-          progress_ms: data.progress_ms
+          progress_ms: data.progress_ms,
+          no_data: false /* We need to "reset" the boolean, in case the
+                            user does not give F5 and has opened his Spotify. */
         });
       }
     });
   }
 
-
-
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        
+      
           {!this.state.token && (
             <a
               className="btn btn--loginApp-link"
@@ -72,18 +99,20 @@ getCurrentlyPlaying(token) {
               )}&response_type=token&show_dialog=true`}
             >
               Login to Spotify
-            ;
             </a>
           )}
-          {this.state.token && (
+          {this.state.token && !this.state.no_data && (
             <Player
               item={this.state.item}
               is_playing={this.state.is_playing}
-              progress_ms={this.progress_ms}
+              progress_ms={this.state.progress_ms}
             />
           )}
-
-          
+          {this.state.no_data && (
+            <p>
+              You need to be playing a song on Spotify, for something to appear here.
+            </p>
+          )}
         </header>
       </div>
     );
